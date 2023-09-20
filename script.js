309cb3e6892e7d012acd1ba916c04c29a33bb5b1
taewoo <rlaxodn322@gmail.com>
2023-09-20 18:39:46 +0900

    javascript 완료

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
 const numbersDiv = document.querySelector(".numbers");
 const drawButton = document.querySelector("#draw");
-const resetButton = documnet.querySelector("#reset");
+const resetButton = document.querySelector("#reset");
 
-const lottonumbers = [];
-const colors = [];
+const lottoNumbers = [];
+const colors = ["tomato", "teal", "orange", "purple", "blue"];
+const paintNumber = (number) => {
+  const eachNumDiv = document.createElement("div");
+  let colorIndex = Math.floor(number / 10);
+  eachNumDiv.classList.add("eachNum");
+  eachNumDiv.style.backgroundColor = colors[colorIndex];
+  eachNumDiv.textContent = number;
+  numbersDiv.appendChild(eachNumDiv);
+};
+
+drawButton.addEventListener("click", () => {
+  while (lottoNumbers.length < 6) {
+    const ran = Math.floor(Math.random() * 45) + 1;
+    if (lottoNumbers.indexOf(ran) === -1) {
+      lottoNumbers.push(ran);
+      paintNumber(ran);
+    }
+  }
+});
+
+resetButton.addEventListener("click", () => {
+  lottoNumbers.splice(0, 6);
+  numbersDiv.innerHTML = "";
+});
